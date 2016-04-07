eb7b91cb8e822960b07d198215fccfa57a4134dc
Brian Stickney <brianchase055@gmail.com>
2016-04-07 16:36:26 -0500

    Forms

---
+++ Forms/Controllers/TasksController.cs
@@ -0,0 +1,56 @@
+﻿using Forms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forms.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            //    List<Task> tasks = new List<Task>()
+            //    {
+            //        new Task ("Brain", "finish assignment", false),
+            //        new Task ("SETH", "finish assignment", true)
+            //    };
+            List<Task> currentTasks;
+            currentTasks = (List<Task>)Session["tasks"] ?? new List<Task>();
+
+
+            return View(currentTasks);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Task newTask)
+        {
+            var currentTasks = (List<Task>)Session["tasks"];
+            if (ModelState.IsValid)
+            {
+                if (currentTasks == null)
+                {
+                    currentTasks = new List<Task>();
+                }
+                currentTasks.Add(newTask);
+                Session["tasks"] = currentTasks;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+            
+        }
+    }
+}
---
+++ Forms/Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Forms.Models
+{
+    public class Task 
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public bool Completion { get; set; }
+
+        public Task(string name,string discription,bool completion)
+        {
+            Name = name;
+            Discription = discription;
+            Completion = completion;
+        }
+
+        public Task()
+        {
+
+        }
+    }
+}
